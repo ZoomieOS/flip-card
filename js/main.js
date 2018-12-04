9cf3657d79de0cb233457dec5051b3addd21f7e5
ZoomieOS <iwenik@gmail.com>
2018-12-05 02:49:05 +0300

    Add logic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,51 @@
 const cards = document.querySelectorAll('.memory-card');
-
-let hasFlippedCard = false;
- let firstCard, secondCard;
+  let hasFlippedCard = false;
+  let lockBoard = false;
+  let firstCard, secondCard;
 
   function flipCard() {
-   this.classList.add('flip');
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
-   if (!hasFlippedCard) {
-     hasFlippedCard = true;
-     firstCard = this;
-   }
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;
+      return;
+    }
+
+    secondCard = this;
+
+    checkForMatch();
   }
-cards.forEach(card => card.addEventListener('click', flipCard));
+
+  function checkForMatch() {
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+    isMatch ? disableCards() : unflipCards();
+  }
+
+  function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+  }
+
+  function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+
+      resetBoard();
+    }, 1500);
+  }
+
+  function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+  cards.forEach(card => card.addEventListener('click', flipCard));
